ba4e9abf6b8c5bfb6d6f71ba96abbc14f7bc28d5
Jason-Delancey <jason.r.delancey@gmail.com>
2018-03-23 11:04:59 -0400

    modified script to join pc to a domain

--- joinPC2Domain.bat
+++ joinPC2Domain.bat
@@ -1,12 +1,12 @@
-echo off
-
 ::Join a PC to the domain
+::Disable firewall on domain, join pc to domain, enable firewall on domain
 echo Joining a PC to the domain...
+netsh advfirewall set allprofiles state off
 set /p computer1="Please enter the name/IP of the machine to join:"
 set /p user1="Please enter the username of a local admin on the machine:"
 set /p password1="Please enter the password:"
 set /p password2="Please enter the domain admin password:"
 netdom join %computer1% /domain:%ComputerName% /ou:OU=PC_Lab_Environment1,DC=home,DC=jrd2172 /userd:%USERNAME% /passwordd:%password2% /usero:%user1% /passwordo:%password1% /reboot
+netsh advfirewall set allprofiles state on
 
-echo All done...
 pause
